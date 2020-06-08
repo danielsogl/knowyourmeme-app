0d2d8afeef29b51c72dc3b7d3e5a448f09f2e2c9
Daniel Sogl <mytechde@outlook.com>
2020-06-08 11:33:28 +0200

    refactor(app): remove ionic native

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,23 @@
 import { Component } from '@angular/core';
-
+import { Plugins, StatusBarStyle } from '@capacitor/core';
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+const { StatusBar, SplashScreen } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor(private platform: Platform) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      StatusBar.setStyle({ style: StatusBarStyle.Light });
+      SplashScreen.hide();
     });
   }
 }
